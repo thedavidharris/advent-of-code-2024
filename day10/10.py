c80dd731173c5d7982456887863e8d49c065cd32
David Harris <davidaharris@outlook.com>
2024-12-16 11:17:30 -0500

    Simpler day 10

--- day10/10.py
+++ day10/10.py
@@ -2,20 +2,14 @@
 
 from collections import defaultdict
 from aocd import get_data, submit
-from collections import defaultdict
-from itertools import product
 import sys
 
 
 data = get_data(day=10, year=2024).splitlines()
 # data = open("test.txt").read().splitlines()
 
-input_grid = []
-start_points = []
-end_points = []
+grid = {}
 
-
-# Parse input
 
 def set_to_int_min_if_not_int(value):
     # Hacky function to cover for putting dots in the test inputs
@@ -26,75 +20,30 @@ def set_to_int_min_if_not_int(value):
         return -sys.maxsize - 1
 
 
-for line in data:
-    input_row = [set_to_int_min_if_not_int(x) for x in list(line)]
-    input_grid.append(input_row)
+for i, row in enumerate(data):
+    for j, cell in enumerate(row):
+        grid[complex(i, j)] = set_to_int_min_if_not_int(cell)
 
-for i, line in enumerate(input_grid):
-    for j, cell in enumerate(line):
-        if cell == 0:
-            start_points += [(i, j)]
-        if cell == 9:
-            end_points += [(i, j)]
+DIRECTIONS = [1, -1, 1j, -1j]
 
 
-# DFS to find all valid paths from start to end
-def find_all_paths(grid, start, end):
-    rows, cols = len(grid), len(grid[0])
-    paths = []
-
-    def dfs(x, y, path, visited):
-        if not (0 <= x < rows and 0 <= y < cols):
-            return
-
-        value = grid[x][y]
-        if (x, y) in visited or (path and value != grid[path[-1][0]][path[-1][1]] + 1):
-            return
-
-        if (x, y) == end:
-            paths.append(path + [(x, y)])
-            return
-
-        path.append((x, y))
-        visited.add((x, y))
-
-        # Explore all four directions
-        dfs(x + 1, y, path, visited)
-        dfs(x - 1, y, path, visited)
-        dfs(x, y + 1, path, visited)
-        dfs(x, y - 1, path, visited)
-
-        # Backtrack
-        path.pop()
-        visited.remove((x, y))
-
-    dfs(start[0], start[1], [], set())
-    return paths
+def search_trailhead(grid, pos, seen, height):
+    if pos in grid and grid[pos] == height:
+        if height < 9 or (part == "a" and pos in seen):
+            # Explore all four directions
+            return sum(search_trailhead(grid, pos + n, seen, height + 1) for n in DIRECTIONS)
+        seen.add(pos)
+        return 1
+    return 0
 
 
-# Part 1
-nines_reached = []
-for start in start_points:
-    nines_reached_for_trailhead = set()
-    for end in end_points:
-        paths = find_all_paths(input_grid, start, end)
-        for path in paths:
-            nines_reached_for_trailhead.add(path[-1])
-    nines_reached += nines_reached_for_trailhead
+def calculate_trailhead_scores(grid, part):
+    """Calculate the sum of scores for all trailheads."""
+    return sum(search_trailhead(grid, pos, set(), 0) for pos in grid if grid[pos] == 0)
 
-part1 = len(nines_reached)
-print(f"Part 1: {part1}")
-submit(part1, part="a", day=10, year=2024)
 
-# Part 2
-
-total_rating = 0
-for start in start_points:
-    rating = 0
-    for end in end_points:
-        paths = find_all_paths(input_grid, start, end)
-        rating += len(paths)
-    total_rating += rating
-
-print(f"Part 2: {total_rating}")
-submit(total_rating, part="b", day=10, year=2024)
+# Calculate scores for both parts
+for part in ["a", "b"]:
+    score = calculate_trailhead_scores(grid, part)
+    print(f"Part {part}: {score}")
+    submit(score, part=part, day=10, year=2024)
